c00e379227d557d7ba231e5cceca9da7ccc1cefd
Blazebrain <davidadegoke16@gmail.com>
2021-11-05 14:02:51 +0100

    Go loops 1 trying out a little function

--- main.go
+++ main.go
@@ -5,8 +5,13 @@ import "fmt"
 
 
 func main (){
-	for i, j:= 0, 1; i<5; i, j = i+1, j + 1{
-		fmt.Println(i, j)
+	for i:= 0; i<5; i++ {
+		fmt.Println(i) 
+		if i%2 == 0{
+			i /=2
+		} else {
+			i = 2*i +1 
+		}
 	}
 }
 
