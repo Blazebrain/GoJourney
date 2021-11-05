eac8341ddea34a22ac4a76de67ae957dab7770e2
Blazebrain <davidadegoke16@gmail.com>
2021-11-05 21:01:06 +0100

    Defer function in go

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
+import "fmt"
+
 
 
 func main (){
 	//Defer : invoke function but defer it's applicationi to later
-	//Panic: point where application enters stae where it cannot continue to run
-	//Recover: how to save the application when it panics
-}
+		fmt.Println("start")
+		defer fmt.Println("middle")
+		fmt.Println("end")
+
+}	
 
 
