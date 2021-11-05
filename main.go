4373b6d01b903a80658dc419cc83861f0777be6e
Blazebrain <davidadegoke16@gmail.com>
2021-11-05 13:08:22 +0100

    switch statement multiple condition 2 fallthrough

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import "fmt"
 
 
 func main (){
-	switch i:= 2+3; i {
-	case 1, 5, 7: 
+	i:= 2+3; 
+	switch  {
+	case i<=10:  
 			fmt.Println("the numbers are odd")
-	case 2, 4, 6, 8: 
+			fallthrough
+	case i<=20: 
 			fmt.Println("the numbers are even")
 	default:
 			fmt.Println("neither or higher scope")
