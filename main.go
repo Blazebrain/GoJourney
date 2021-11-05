b3960c4d255e430203a08804d837f6801eda6502
Blazebrain <davidadegoke16@gmail.com>
2021-11-05 13:22:17 +0100

    Go loops 1 simple loops

--- main.go
+++ main.go
@@ -5,21 +5,8 @@ import "fmt"
 
 
 func main (){
-
-	// Performing Type switchning in Go
-	var i interface {} = 1
-	switch i.(type) {
-	case int:
-		fmt.Println("i is an int")
-		break
-		fmt.Println("i is an int") //Code will break and this code won't print
-	case float64:
-		fmt.Println("i is a float64")
-	case string:
-		fmt.Println("i is a string")
-	default: 
-	fmt.Println("i is another type")
-		 
+	for i:= 0; i<5; i++ {
+		fmt.Println(i)
 	}
 }
 
