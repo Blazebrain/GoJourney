68ea3abaab05466f7bd6c712ac0d22a9afef28ee
Blazebrain <davidadegoke16@gmail.com>
2021-11-05 13:13:36 +0100

    switch statement multiple condition 2 type switching

--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import "fmt"
 
 
+
 func main (){
-	i:= 2+3; 
-	switch  {
-	case i<=10:  
-			fmt.Println("the numbers are odd")
-			fallthrough
-	case i<=20: 
-			fmt.Println("the numbers are even")
-	default:
-			fmt.Println("neither or higher scope")
+
+	// Performing Type switchning in Go
+	var i interface {} = 1
+	switch i.(type) {
+	case int:
+		fmt.Println("i is an int")
+	case float64:
+		fmt.Println("i is a float64")
+	case string:
+		fmt.Println("i is a string")
+	default: 
+	fmt.Println("i is another type")
 		 
 	}
 }
