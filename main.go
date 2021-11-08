10aa5e23650eabf4bdac55cd2cb7bdf76e4055c2
Blazebrain <davidadegoke16@gmail.com>
2021-11-08 04:22:35 +0100

    Using a for loop to manipulate a function

--- main.go
+++ main.go
@@ -6,15 +6,19 @@ import (
 
 
 
-func main (){
-	for i:= 0; i<=5; i++{
-		sayMessage("Hello Go", i)
+func main(){
+	sum(1,2,3,4,5)
+}
 
+func sum(values ...int){
+	fmt.Println(values)
+	result :=0
+
+
+	for i:=0; i < len(values); i++{
+		println(values[i])
+		result += values[i]
 	}
 
+	fmt.Println("The result is", result)
 }
-func sayMessage(msg string, i int){
-	fmt.Println(msg)
-	fmt.Println("The value of index is", i)
-}
-
