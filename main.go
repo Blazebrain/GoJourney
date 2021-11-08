b65fff483e6d0b9569edc1ddb388549865b1995c
Blazebrain <davidadegoke16@gmail.com>
2021-11-08 05:39:43 +0100

    using wait group with go routines

--- main.go
+++ main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-
+var wg = sync.WaitGroup{}
 
 func main(){
 	var msg = "hello"
-
+	wg.Add(1)
 	go func(msg string){
 		fmt.Println(msg)
+		wg.Done()
 	}(msg)
 	msg = "Goodbye"
-	time.Sleep(100*time.Millisecond)
+	wg.Wait()
 }
