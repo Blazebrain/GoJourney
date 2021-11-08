619ccd0272803c9ae6455a34b34809d42ee145dd
Blazebrain <davidadegoke16@gmail.com>
2021-11-08 06:07:27 +0100

    this would throw an error as the goroutines have been made send only and receive only

--- main.go
+++ main.go
@@ -11,15 +11,22 @@ func main(){
 	ch := make(chan int)
 	wg.Add(2)
 	//Receiving goroutine
-	go func (){
+	//the parameter makes it a receive only channel goroutine
+	//so data can't be sent from this goroutine
+	go func (ch <-chan int){
 		i:= <- ch
 		fmt.Println(i)
-		wg.Done()
-	}()
-	//sending goroutine
-	go func (){
 		ch <- 42
 		wg.Done()
-	}()
+	}(ch)
+	//sending goroutine
+	//the parameter makes it a send only channel goroutine, 
+	//so data can't be received in this goroutine
+	go func (ch chan <- int){
+		ch <- 42
+		a := <- ch
+		print(a)
+		wg.Done()
+	}(ch)
 	wg.Wait()
 }
