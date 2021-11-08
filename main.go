8624b103d74b61a1062c27ab40b6d64bf780a8f3
Blazebrain <davidadegoke16@gmail.com>
2021-11-08 06:08:44 +0100

    all fixed now

--- main.go
+++ main.go
@@ -11,12 +11,12 @@ func main(){
 	ch := make(chan int)
 	wg.Add(2)
 	//Receiving goroutine
-	//the parameter makes it a receive only channel goroutine
+	//the parameter makes it a receive only goroutine
 	//so data can't be sent from this goroutine
 	go func (ch <-chan int){
 		i:= <- ch
 		fmt.Println(i)
-		ch <- 42
+	
 		wg.Done()
 	}(ch)
 	//sending goroutine
@@ -24,8 +24,6 @@ func main(){
 	//so data can't be received in this goroutine
 	go func (ch chan <- int){
 		ch <- 42
-		a := <- ch
-		print(a)
 		wg.Done()
 	}(ch)
 	wg.Wait()
