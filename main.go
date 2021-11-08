379237e11ec4e9849e61869209731e8af641562e
Blazebrain <davidadegoke16@gmail.com>
2021-11-08 04:35:40 +0100

    Returning an error message instead of panic when an input breaks the app

--- main.go
+++ main.go
@@ -7,14 +7,19 @@ import (
 
 
 func main(){
-	s:=	sum(1,2,3,4,5)
-		fmt.Println(s)
+	d, err:= divide(5.0, 0.0)
+
+	if err !=nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(d)
 }
 
-func sum(values ...int)(result int){
-	fmt.Println(values)
-	for _,v:= range values{
-		result += v
-	}
-	return 
+
+func divide(a, b float64)(float64, error){
+		if b == 0.0{
+			return 0.0, fmt.Errorf("Cannot divide by zero")
+		}
+		return a/b, nil
 }
